b27b65d7c6a1ac3cbee31b9d29d18adb3a72d468
Camila Manita <camilamanita3@gmail.com>
2023-05-10 21:05:47 -0300

    getAll done

--- src/controllers/getAll.controllers.js
+++ src/controllers/getAll.controllers.js
@@ -1,10 +1,10 @@
+const { Pokemon } = require('../db');
 const { API_URL } = process.env;
 const axios = require("axios");
 
 const getAllPokemons = async () => {
-  const urlBase = `${API_URL}`;
   
-  //Intento 2
+  //Traigo de la API
   const getPokemonDetails = (url) => {
     //Obtengo los detalles de un pokemon
     return axios.get(url)
@@ -13,57 +13,35 @@ const getAllPokemons = async () => {
   }
   
   //Obtengo la lista de URLs de todos los pokemon
-  const response = await axios.get(urlBase);
+  const response = await axios.get(API_URL);
   const urls = response.data.results.map(pokemon => pokemon.url);
 
   //Obtengo los detalles de todos los pokemon
   const pokemonDetails = await Promise.all(urls.map(url => getPokemonDetails(url)));
 
   //Convertimos el array de detalles
-  const pokemons = pokemonDetails.map(pokemon => {
-    const stats = {};
-    pokemon.stats.forEach(stat => {
-      switch (stat.stat.name) {
-        case 'hp':
-          stats.hp = stat.base_stat;
-          break;
-        case 'attack':
-          stats.attack = stat.base_stat;
-          break;
-          case 'defense':
-            stats.defense = stat.base_stat;
-            break;
-            case 'speed':
-          stats.speed = stat.base_stat;
-          break;
-      }
-    });
-
+  const pokemonsAPI = pokemonDetails.map(pokemon => {
     return {
       id: pokemon.id,
       name: pokemon.name,
+      image: pokemon.sprites.other.home.front_default,
+      hp: pokemon.stats[0].base_stat,
+      attack: pokemon.stats[1].base_stat,
+      defense: pokemon.stats[2].base_stat,
+      speed: pokemon.stats[5].base_stat,
       height: pokemon.height,
       weight: pokemon.weight,
       types: pokemon.types.map(type => type.type.name),
-      abilities: pokemon.abilities.map(ability => ability.ability.name),
-      image: pokemon.sprites.front_default,
-      stats
     };
   });
+  //Busco en la base de datos
+  const pokemonsDB = await Pokemon.findAll()
 
-  console.log(pokemons);
+  //Guardo ambas respuestas en un array
+  const allPokemons = [...pokemonsAPI, ...pokemonsDB]
+
+  return allPokemons;
 };
 
 module.exports = getAllPokemons;
 
-//FUNCION AUXILIAR Y POR LAS DUDAS DE QUE LA OTRA ESTÉ MAL
-      //Intento 1
-      // const response = await axios.get(`${API_URL}?limit=50`);
-      // if (!response.data) throw new Error("No tenes personaje en el pokedex");
-      // console.log(response.data.results);
-      // axios.get(urlBase).then(response => { //con esto hago un array de urls
-      //   const urls = response.data.results.map(pokemon => urlBase + pokemon.name);
-      //   console.log(urls);
-      // })
-      // .catch(error => console.log(error));
-      // return response.data.results //Retorno un array de objetos con el nombre y la url de c/pokemon
