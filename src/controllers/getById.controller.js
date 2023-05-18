0c85662c29484e5ace9686518ce75153e92c2315
Camila Manita <camilamanita3@gmail.com>
2023-05-17 22:18:45 -0300

    new update

--- src/controllers/getById.controller.js
+++ src/controllers/getById.controller.js
@@ -1,58 +1,11 @@
-require('dotenv').config();
-const { API_URL } = process.env;
-const axios = require("axios");
-const { Pokemon, Type } = require('../db');
-
-const getIdDB = async (id) => {
-  const pokemonDB = await Pokemon.findByPk(
-    id, {
-      include: {
-        model: Type,
-        attributes: ["id", 'name'],
-        through: {
-          attributes: [],
-        }
-      }
-    }
-  )
-  return pokemonDB;
-}
-
-const getIdAPI = async (id) => {
-  try {
-    const pokemonAPI = []
-    const response = await axios.get(`${API_URL}/${id}`);
-    const {name, sprites, stats, height, weight, types} = response.data;
-    if(response){
-      pokemonAPI.push({
-          id: response.id, //Esto se coloca así porque en el destructuring me tira error
-          name, 
-          image: sprites.other.home.front_default,
-          hp: stats[0].base_stat,
-          attack: stats[1].base_stat,
-          defense: stats[2].base_stat,
-          speed: stats[5].base_stat,
-          height: height,
-          weight: weight,
-          types: types.map((type) => type.type.name),
-      });
-    }
-    return pokemonAPI; 
-  } catch (error) {
-    return [];
-  }
-}
+const getAllPokemons = require('./getAll.controllers');
 
 const getPokemonById = async (id) => {
-  if(!isNaN(id)){
-    const pokemonAPI = await getIdAPI(id);
-    if(!pokemonAPI.length) throw new Error('El pokemon no existe')
-    return pokemonAPI
-  } else {
-    const pokemonDB = await getIdDB(id);
-    if(!pokemonDB) throw new Error('El pokemon no existe en la DB')
-    return pokemonDB;
-  }
-};
+    const response = await getAllPokemons();
+    if(!response) throw new Error('No existen los pokemones')
+
+    const pokemonFound = response.find(poke => poke.id == id);
+    return pokemonFound;
+}
 
 module.exports = getPokemonById;
--- src/controllers/getByName.controllers.js
+++ src/controllers/getByName.controllers.js
@@ -1,59 +1,11 @@
-const axios = require("axios");
-const { API_URL } = process.env;
-const {Op} = require('sequelize');
-const {Pokemon, Type} = require('../db')
+const getAllPokemons = require('./getAll.controllers');
 
-const getNameDB = async (name) => {
-  const pokemonDB = await Pokemon.findAll({
-    where: {
-      name: {[Op.iLike]: `%${name}%`}
-    },
-    include: {
-      model: Type,
-      attributes: ["name"],
-      through: {
-        attributes: [],
-      },
-    },
-  })
-  
-  return [...pokemonDB]
+const getName = async (name) => {
+    const response = await getAllPokemons();
+    if(!response) throw new Error('No existen los pokemones')
+
+    const pokemonFound = response.find(poke => poke.name == name);
+    return pokemonFound;
 }
 
-const getNameAPI = async (name) => {
-  try {
-    const pokemonAPI = []
-    const response = (await axios.get(`${API_URL}/${name}`)).data;
-    const {id, sprites, stats, height, weight, types} = response;
-    if(response){
-      pokemonAPI.push({
-          id,
-          name: response.name, //Esto se coloca así porque en el destructuring me tira error
-          image: sprites.other.home.front_default,
-          hp: stats[0].base_stat,
-          attack: stats[1].base_stat,
-          defense: stats[2].base_stat,
-          speed: stats[5].base_stat,
-          height: height,
-          weight: weight,
-          types: types.map((type) => type.type.name),
-      });
-    }
-    return pokemonAPI; 
-  } catch (error) {
-    return [];
-  }
-}
-
-const getPokemonByName = async (name) => {
-  const pokemonDB = await getNameDB(name);
-  const pokemonAPI = await getNameAPI(name);
-
-  const response = [...pokemonDB, ...pokemonAPI];
-
-  if(response.length === 0) return 'El pokemon no existe';
-
-  return response;
-};
-
-module.exports = getPokemonByName;
+module.exports = getName;
